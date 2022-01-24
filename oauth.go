246354368655e02e7baee7f143d68242e1535de7
IJY <dunebi1030123@gmail.com>
2022-01-24 15:25:56 +0900

    v1.0.0

--- oauth.go
+++ oauth.go
@@ -3,14 +3,26 @@ package oauth
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 
+	"github.com/gin-gonic/gin"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/facebook"
 	"golang.org/x/oauth2/github"
 	"golang.org/x/oauth2/google"
+	"gorm.io/gorm"
 )
+
+type Account struct {
+	gorm.Model
+	Email string `json:"email"`
+	CA    string
+}
 
 func RandToken() string {
 	b := make([]byte, 32)
@@ -38,4 +50,101 @@ func GetOauth2Config(CA string) *oauth2.Config {
 	}
 
 	return oauth2Config
+}
+
+func Login(c *gin.Context) {
+	param := c.Param("CA")
+	splitArr := strings.Split(param, "/")
+	CA := strings.ToUpper(splitArr[1]) // CA(google, facebook, github)
+
+	// 지정한 인증 사이트가 아닌 경우
+	if (CA != "GOOGLE") && (CA != "FACEBOOK") && (CA != "GITHUB") {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"input CA": CA,
+			"msg":      "use google, facebook or github",
+		})
+		return
+	}
+
+	os.Setenv("CA", CA) // Redirect Handler에서도 oauthConfig를 가져와야 하므로 환경변수로 설정
+	oauth2Config := GetOauth2Config(CA)
+
+	url := oauth2Config.AuthCodeURL(RandToken(), oauth2.AccessTypeOffline)
+	c.Redirect(http.StatusTemporaryRedirect, url)
+}
+
+func LoginCallback(c *gin.Context, db *gorm.DB) {
+	apiURL := map[string]string{
+		"GOOGLE":   "https://www.googleapis.com/oauth2/v3/userinfo",
+		"FACEBOOK": "https://graph.facebook.com/me?locale=en_US&fields=name,email",
+		"GITHUB":   "https://api.github.com/user",
+	}
+
+	CA := os.Getenv("CA")
+	oauth2Config := GetOauth2Config(CA)
+
+	code := c.Query("code")
+
+	token, err := oauth2Config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"msg": "Error on get token",
+		})
+		return
+	}
+
+	client := oauth2Config.Client(oauth2.NoContext, token)
+	userInfoResp, err := client.Get(apiURL[CA])
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg":   "Error on get usrInfo",
+			"error": err.Error(),
+		})
+	}
+	defer userInfoResp.Body.Close()
+	userInfo, err := ioutil.ReadAll(userInfoResp.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": "Error on read userinfo",
+		})
+		c.Abort()
+		return
+	}
+
+	var account, dbAccount Account
+	json.Unmarshal(userInfo, &account)
+
+	email := account.Email
+	db.Where("Email = ? AND CA = ?", email, CA).Find(&dbAccount)
+
+	// DB 계정이 없다면 Register
+	if dbAccount.ID == 0 { // No Email Info. Auto register and request re-login
+		account.CA = CA
+		result := db.Create(&account)
+		if result.Error != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"msg": "Error on creating Account",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"msg":         "New account created. Please re-login",
+			"accountInfo": account,
+		})
+		return
+	}
+
+	// DB에 계정이 있는 경우. JWT토큰을 생성하여 반환
+	jwtToken, err := GenerateToken(email)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"msg": "Error on Create JWT token",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"JWT": jwtToken,
+	})
 }
