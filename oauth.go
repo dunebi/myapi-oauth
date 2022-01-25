21c90827827d277c12824238dfdc2d6e03663cea
IJY <dunebi1030123@gmail.com>
2022-01-25 11:12:45 +0900

    핸들러 수정

--- oauth.go
+++ oauth.go
@@ -52,38 +52,42 @@ func GetOauth2Config(CA string) *oauth2.Config {
 	return oauth2Config
 }
 
-func Login(c *gin.Context) {
-	param := c.Param("CA")
-	splitArr := strings.Split(param, "/")
-	CA := strings.ToUpper(splitArr[1]) // CA(google, facebook, github)
+func Login() func(c *gin.Context) {
+	var oauth2Config *oauth2.Config
+	CA := ""
 
-	// 지정한 인증 사이트가 아닌 경우
-	if (CA != "GOOGLE") && (CA != "FACEBOOK") && (CA != "GITHUB") {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"input CA": CA,
-			"msg":      "use google, facebook or github",
-		})
-		return
+	return func(c *gin.Context) {
+		param := c.Param("CA")
+		splitArr := strings.Split(param, "/")
+		newCA := strings.ToUpper(splitArr[1]) // CA(google, facebook, github)
+
+		// 지정한 인증 사이트가 아닌 경우
+		if (CA != "GOOGLE") && (CA != "FACEBOOK") && (CA != "GITHUB") {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"input CA": CA,
+				"msg":      "use google, facebook or github",
+			})
+			return
+		}
+
+		os.Setenv("CA", newCA) // Redirect Handler에서도 oauthConfig를 가져와야 하므로 환경변수로 설정
+		if CA != newCA {
+			oauth2Config = GetOauth2Config(newCA)
+			CA = newCA
+		}
+
+		url := oauth2Config.AuthCodeURL(RandToken(), oauth2.AccessTypeOffline)
+		c.Redirect(http.StatusTemporaryRedirect, url)
 	}
-
-	os.Setenv("CA", CA) // Redirect Handler에서도 oauthConfig를 가져와야 하므로 환경변수로 설정
-	oauth2Config := GetOauth2Config(CA)
-
-	url := oauth2Config.AuthCodeURL(RandToken(), oauth2.AccessTypeOffline)
-	c.Redirect(http.StatusTemporaryRedirect, url)
 }
 
+/*
 type AA interface {
 	A()
 }
+*/
 
-func LoginCallback(c *gin.Context, db *gorm.DB) {
-	process := loginCallbackProcess(c, db)
-
-	process()
-}
-
-func loginCallbackProcess(c *gin.Context, db *gorm.DB) func() {
+func LoginCallback(db *gorm.DB) func(c *gin.Context) {
 	apiURL := map[string]string{
 		"GOOGLE":   "https://www.googleapis.com/oauth2/v3/userinfo",
 		"FACEBOOK": "https://graph.facebook.com/me?locale=en_US&fields=name,email",
@@ -92,7 +96,7 @@ func loginCallbackProcess(c *gin.Context, db *gorm.DB) func() {
 	var oauth2Config *oauth2.Config
 	CA := ""
 
-	return func() {
+	return func(c *gin.Context) {
 		newCA := os.Getenv("CA")
 		if CA != newCA {
 			oauth2Config = GetOauth2Config(newCA)
